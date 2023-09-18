869eafd3016161dd80f88cd2f007271241c917c4
William Spriggs <Willspri@gmail.com>
2023-09-18 14:41:33 +1200

    Updated

--- AuthorAttribute/AuthorAttribute.py
+++ AuthorAttribute/AuthorAttribute.py
@@ -1,6 +1,6 @@
 import requests
 import numpy as np
-#import ntlk
+
 
 # Define a list holding all characters to analyze character transitions 
 state = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "  ", ",", "'"]
